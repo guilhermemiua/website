229aaa79c2f9f5527ad3d2c3f31d7e1754b11061
guilhermemiua <guilhermemiua@gmail.com>
2026-06-16 22:40:59 -0300

    feat: add links to constant folder

---
+++ src/constants/links.ts
@@ -0,0 +1,14 @@
+export const LINKS = {
+  github: {
+    url: "https://github.com/guilhermemiua",
+    icon: "ph:github-logo",
+  },
+  linkedin: {
+    url: "https://www.linkedin.com/in/guilherme-eiti-akita-miua-903808187/?skipRedirect=true",
+    icon: "ph:linkedin-logo",
+  },
+  spotify: {
+    url: "https://open.spotify.com/user/l0w9k27zzjsuzq9efd5b3iuv6?si=d72057666b124824",
+    icon: "ph:spotify-logo",
+  },
+};
