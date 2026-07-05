4045e646d38823e5be738f790d32fa1a5c8d78d5
guilhermemiua <guilhermemiua@gmail.com>
2026-07-05 19:31:00 -0300

    feat: add wrangler, default to node v24

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,19 @@ import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   base: "/website/",
   trailingSlash: "always",
   site: "https://guilhermemiua.github.io",
   integrations: [icon()],
+
   vite: {
     plugins: [tailwindcss()],
   },
+
   fonts: [
     {
       provider: fontProviders.google(),
@@ -19,4 +23,6 @@ export default defineConfig({
       cssVariable: "--font-inter",
     },
   ],
+
+  adapter: cloudflare(),
 });
